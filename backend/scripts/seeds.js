2968de69f8f4fe8e55992c8156a08b90ed33d892
Ariel Mannes <arielmannes@gmail.com>
2023-01-24 00:56:36 +0200

    seed one of each

--- backend/scripts/seeds.js
+++ backend/scripts/seeds.js
@@ -7,57 +7,39 @@ require("../models/Comment");
 
 var User = mongoose.model("User");
 var Item = mongoose.model("Item");
-var Comment = mongoose.model("Comment");
+var Comment = mongoose.model("comment");
 
 
 var items = ["bike", "car", "house", "phone", "laptop"];
 var description = "This is a mock description. ";
 var adjectives = ["small", "luxurious", "big", "vintage", "new"];
-var aComment = "wow, what an item!";
-
 var randomNumber = (number)=>{
     return Math.floor(Math.random() * number);
 };
 
-var seedUserWithItems = async (id)=>{
+var seedDB = async ()=>{
     var testUser = new User();
-    testUser.username = `testUser${id}`;
-    testUser.email = `notValid${id}@yahoo.com`
+    let seed = randomNumber(70);
+    testUser.username = `testUser${seed}`;
+    testUser.email = `notValid${seed}@yahoo.com`
     testUser.setPassword("111");
     await testUser.save();
-    const item = await saveItem(testUser);
-    await createComment(testUser, item);
-    
-    return;
-}
-
-async function saveItem(testUser) {
-    let anItem = new Item();
-    anItem.title = `${adjectives[randomNumber(5)]} ${items[randomNumber(5)]}`;
+    let anItem = new Item;
+    anItem.title = `${adjectives[randomNumber()]} ${items[randomNumber()]}`;
     anItem.description = description;
     anItem.image = `https://picsum.photos/id/${randomNumber(200)}/200`;
     anItem.seller = testUser;
-    anItem.tagList = ["#test"];
     await anItem.save();
-    return anItem;
-}
-async function createComment(user, item){
-    let aComment = new Comment('test comment');
-    aComment.item = item;
-    Comment.seller = user;
+    let aComment = new Comment("some test comment");
+    aComment.item = anItem;
+    aComment.seller = testUser;
     await aComment.save();
-    return;
-}
-async function seed() {
-    mongoose.connect(process.env.MONGODB_URI);
-    let allPromises = [];
-    for (let index = 0; index < 101; index++) {
-        allPromises.push(seedUserWithItems());
-    }
-    await Promise.allSettled(allPromises);
-    await mongoose.disconnect();
     return;
     
 }
 
-seed();
+mongoose.connect(process.env.MONGODB_URI);
+
+await seedDB();
+
+mongoose.disconnect();
